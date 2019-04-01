bd42e49023330a50e7799c54fd6e9925a0498c4e
Greg Nemes <mail@gregnemes.com>
2019-04-01 11:18:49 -0400

    added bio

--- themes/work-shop/page-about.php
+++ themes/work-shop/page-about.php
@@ -215,166 +215,169 @@
 							<div class="col-sm-5 col-sm-offset-1">
 								<h3 class="bold brand m1"><?php echo $person['person_name']; ?>
 								<br/>
-								 <?php echo $person['person_role']; ?>
+								<?php echo $person['person_role']; ?>
 								<h3>
-								<h4>
-									<a href="mailto:<?php echo $person['person_email']; ?>" target="_blank">
-										<?php echo $person['person_email']; ?>
-									</a>
-								</h4>
+									<h4>
+										<a href="mailto:<?php echo $person['person_email']; ?>" target="_blank">
+											<?php echo $person['person_email']; ?>
+										</a>
+									</h4>
+									<h5>
+										<?php echo $person['people_bio']; ?>
+									</h5>
+								</div>
 							</div>
 						</div>
-					</div>
 
-					<?php
+						<?php
 
 					//if ( $current_i % 3 == 0 ) $current .= $row_separator_open;
 
-					$current .= '<div class="'.ws_parity( $current_i, 'left-side', 'right-side')
-					.  (($current_i == 0) ? " col-sm-offset-2 " : " col-sm-offset-0")
-					.  ' col-sm-3 person">'
+						$current .= '<div class="'.ws_parity( $current_i, 'left-side', 'right-side')
+						.  (($current_i == 0) ? " col-sm-offset-2 " : " col-sm-offset-0")
+						.  ' col-sm-3 person">'
 							 //.  ws_ifdef_concat('<a href="',ws_decide_link_type($person['person_link']),'" target="_blank">')
-					.  '<img class="m1" src="'.$person['person_image']['sizes']['person'].'">'
-					.  ws_ifdef_concat( '<h3 class="bold brand m1">',$person['person_name'],'<br/> ' )
-					.  ws_ifdef_concat( '',$person['person_role'],'</h3>' )
-					.  ws_ifdef_concat( '<h5>',$person['people_bio'],'</h5>')						     
-					.  ws_ifdef_concat( '<h5>',$person['person_phone_number'],'</h5>' )
+						.  '<img class="m1" src="'.$person['person_image']['sizes']['person'].'">'
+						.  ws_ifdef_concat( '<h3 class="bold brand m1">',$person['person_name'],'<br/> ' )
+						.  ws_ifdef_concat( '',$person['person_role'],'</h3>' )
+						.  ws_ifdef_concat( '<h5>',$person['people_bio'],'</h5>')						     
+						.  ws_ifdef_concat( '<h5>',$person['person_phone_number'],'</h5>' )
 						     //.  ws_ifdef_do($person['person_link'], '</a>')
-					.  ws_ifdef_concat( '<h4><a href="mailto:',$person['person_email'],'" target="_blank">' )
-					.  ws_ifdef_concat( '',$person['person_email'],'</a></h4>' )
-					.  '</div>';
+						.  ws_ifdef_concat( '<h4><a href="mailto:',$person['person_email'],'" target="_blank">' )
+						.  ws_ifdef_concat( '',$person['person_email'],'</a></h4>' )
+						.  '</div>';
 
 					//if ( $current_i % 2 == 1 || $current_i == $total_current - 1 ) $current .= $row_separator_close;
 
-					$current_i++;
+						$current_i++;
 
-				} else {
-					
-					if ( $former_i % 3 == 0 ) $former .= $row_separator_open;
+					} else {
+						
+						if ( $former_i % 3 == 0 ) $former .= $row_separator_open;
 
-					$former .= '<div class="'.ws_parity( $former_i, 'left', 'right').'">'
-					.  ws_ifdef_concat('<a href="',ws_decide_link_type( $person['person_link'] ),'" >')
-					.  '<img src="'.$person['person_image']['sizes']['full'].'">'
-					.  ws_ifdef_concat( '<h3>',$person['person_name'],', ' )
-					.  ws_ifdef_concat( '',$person['person_role'],'</h3>' )
-					.  ws_ifdef_concat( '<h4>',$person['person_phone_number'],'</h4>' )
-					.  ws_ifdef_concat( '<h4>',$person['person_email'],'</h4>' )
-					.  ws_ifdef_concat( '<p>',$person['people_bio'],'</p>')
-					.  ws_ifdef_do($person['person_link'], '</a>')
-					.  '</div>';
+						$former .= '<div class="'.ws_parity( $former_i, 'left', 'right').'">'
+						.  ws_ifdef_concat('<a href="',ws_decide_link_type( $person['person_link'] ),'" >')
+						.  '<img src="'.$person['person_image']['sizes']['full'].'">'
+						.  ws_ifdef_concat( '<h3>',$person['person_name'],', ' )
+						.  ws_ifdef_concat( '',$person['person_role'],'</h3>' )
+						.  ws_ifdef_concat( '<h4>',$person['person_phone_number'],'</h4>' )
+						.  ws_ifdef_concat( '<h4>',$person['person_email'],'</h4>' )
+						.  ws_ifdef_concat( '<p>',$person['people_bio'],'</p>')
+						.  ws_ifdef_do($person['person_link'], '</a>')
+						.  '</div>';
 
-					$former_i++;
+						$former_i++;
 
-					if ( $former_i % 2 == 1 || $former_i == $total_former - 1 ) $former .= $row_separator_close;
+						if ( $former_i % 2 == 1 || $former_i == $total_former - 1 ) $former .= $row_separator_close;
 
+					}
 				}
-			}
 
 			//echo $current . '</div>';
 			//echo $former . '</div>';
 
-			echo ws_ifdef_concat('<div>',$postpeople,'</div>');
+				echo ws_ifdef_concat('<div>',$postpeople,'</div>');
 
-			?>
+				?>
+
+			</div>
+			
+			<div class="row hidden">
+				<div class="col-sm-8 col-sm-offset-2">
+					<h3 class=" m30">
+						Greg and Nic met in 2012 and worked together on the two projects that would lay the foundation for Work-Shop, the <a href="<?php echo get_the_permalink(106); ?>"><?php echo get_the_title(106); ?></a> and the <a href="<?php echo get_the_permalink(30); ?>"><?php echo get_the_title(30); ?></a>. Work-Shop was established in the fall of 2013, as a way of formalizing their working relationship and continuing their explorations into new ways of making and working.
+					</h3>			
+				</div>
+			</div>
+
+			<div class="row hidden">
+				<div class="col-sm-9 col-sm-offset-2">
+					<h3 class=""><a href="<?php bloginfo('url'); ?>/jobs" class="bold h3">We're hiring! Check out our open positions.</a></h3>
+				</div>
+			</div>
 
 		</div>
+	</section>
+
+
+	<section id="about-place" class="about-6 block min padded hidden">	
 		
-		<div class="row hidden">
-			<div class="col-sm-8 col-sm-offset-2">
-				<h3 class=" m30">
-					Greg and Nic met in 2012 and worked together on the two projects that would lay the foundation for Work-Shop, the <a href="<?php echo get_the_permalink(106); ?>"><?php echo get_the_title(106); ?></a> and the <a href="<?php echo get_the_permalink(30); ?>"><?php echo get_the_title(30); ?></a>. Work-Shop was established in the fall of 2013, as a way of formalizing their working relationship and continuing their explorations into new ways of making and working.
-				</h3>			
+		<div class="block-background">
+		</div>
+
+		<?php 
+
+		$office_image = get_field('footer_office_image', 'option');
+		$office_description = get_field('footer_office_information', 'option');
+
+		$shop_image = get_field('footer_shop_image', 'option');
+		$shop_description = get_field('footer_shop_information', 'option');
+
+		?>
+		
+		<div class="block-background mask-light-light" style="background-image: url('<?php echo $office_image['sizes']['slideshow']; ?>')">	
+		</div>
+
+		<div class="container">
+			<div class="row">
+				
+				<div class="col-sm-4 col-sm-offset-2 office-description">
+					<?php echo ws_ifdef_do( $office_description, '<div>'.$office_description.'</div>'); ?>
+				</div>
+				
+				<div class="col-sm-4 shop-description">
+					<?php echo ws_ifdef_do( $shop_description, '<div>'.$shop_description.'</div>'); ?>
+				</div>
+
 			</div>
 		</div>
 
-		<div class="row hidden">
-			<div class="col-sm-9 col-sm-offset-2">
-				<h3 class=""><a href="<?php bloginfo('url'); ?>/jobs" class="bold h3">We're hiring! Check out our open positions.</a></h3>
+
+	</section>	
+
+
+	<section id="about-values" class="about-3 about-process block padded bg-light">	
+
+		<div class="container">
+
+			<div class="row m90 ">
+				<div class="col-sm-4 col-sm-offset-2"><h2>Make the world a slightly better place, through our work.</h2></div>
+				<div class="col-sm-4 col-sm-offset-1"><h2>Work for clients, and work with people – who we truly believe in.</h2></div>
 			</div>
+			<div class="row m90">
+				<div class="col-sm-4 col-sm-offset-2"><h2>Make things that are built for a constantly changing, evolving world.</h2></div>			
+				<div class="col-sm-4 col-sm-offset-1"><h2>Be open to The New, but don't go hunting for it.</h2></div>
+			</div>
+			<div class="row m90">			
+				<div class="col-sm-5 col-sm-offset-2"><h2>Draw from a diverse set of backgrounds and experiences to find new ways to approach problems.</h2></div>
+				<div class="col-sm-4 col-sm-offset-0"><h2>Collaborate with clients to understand the core problems on each project.</h2></div>			
+			</div>		
+
 		</div>
 
-	</div>
-</section>
+	</section>		
 
 
-<section id="about-place" class="about-6 block min padded hidden">	
-	
-	<div class="block-background">
-	</div>
-
-	<?php 
-
-	$office_image = get_field('footer_office_image', 'option');
-	$office_description = get_field('footer_office_information', 'option');
-
-	$shop_image = get_field('footer_shop_image', 'option');
-	$shop_description = get_field('footer_shop_information', 'option');
-
-	?>
-	
-	<div class="block-background mask-light-light" style="background-image: url('<?php echo $office_image['sizes']['slideshow']; ?>')">	
-	</div>
-
-	<div class="container">
+	<div class="container" id="about-nav-container">
 		<div class="row">
-			
-			<div class="col-sm-4 col-sm-offset-2 office-description">
-				<?php echo ws_ifdef_do( $office_description, '<div>'.$office_description.'</div>'); ?>
-			</div>
-			
-			<div class="col-sm-4 shop-description">
-				<?php echo ws_ifdef_do( $shop_description, '<div>'.$shop_description.'</div>'); ?>
-			</div>
+			<div class="col-sm-3">	
 
+				<div id="about-nav" class="spy falloff">
+					<ul>
+						<li><a href="#about-introduction" class="jump">About</a></li>					
+						<li><a href="#about-process" class="jump">Process</a></li>
+						<li><a href="#about-services" class="jump">Services</a></li>						
+						<li><a href="#about-people" class="jump">Leadership</a></li>
+						<li><a href="#about-values" class="jump">Values</a></li>
+						<li><a href="#contact" class="jump">Contact</a></li>				
+						<li><a href="#invitation" class="jump hidden falloff-link"></a></li>
+
+					</ul>
+				</div>
+
+			</div>
 		</div>
 	</div>
-
-
-</section>	
-
-
-<section id="about-values" class="about-3 about-process block padded bg-light">	
-
-	<div class="container">
-
-		<div class="row m90 ">
-			<div class="col-sm-4 col-sm-offset-2"><h2>Make the world a slightly better place, through our work.</h2></div>
-			<div class="col-sm-4 col-sm-offset-1"><h2>Work for clients, and work with people – who we truly believe in.</h2></div>
-		</div>
-		<div class="row m90">
-			<div class="col-sm-4 col-sm-offset-2"><h2>Make things that are built for a constantly changing, evolving world.</h2></div>			
-			<div class="col-sm-4 col-sm-offset-1"><h2>Be open to The New, but don't go hunting for it.</h2></div>
-		</div>
-		<div class="row m90">			
-			<div class="col-sm-5 col-sm-offset-2"><h2>Draw from a diverse set of backgrounds and experiences to find new ways to approach problems.</h2></div>
-			<div class="col-sm-4 col-sm-offset-0"><h2>Collaborate with clients to understand the core problems on each project.</h2></div>			
-		</div>		
-
-	</div>
-
-</section>		
-
-
-<div class="container" id="about-nav-container">
-	<div class="row">
-		<div class="col-sm-3">	
-
-			<div id="about-nav" class="spy falloff">
-				<ul>
-					<li><a href="#about-introduction" class="jump">About</a></li>					
-					<li><a href="#about-process" class="jump">Process</a></li>
-					<li><a href="#about-services" class="jump">Services</a></li>						
-					<li><a href="#about-people" class="jump">Leadership</a></li>
-					<li><a href="#about-values" class="jump">Values</a></li>
-					<li><a href="#contact" class="jump">Contact</a></li>				
-					<li><a href="#invitation" class="jump hidden falloff-link"></a></li>
-
-				</ul>
-			</div>
-
-		</div>
-	</div>
-</div>
 
 
 </div>	
