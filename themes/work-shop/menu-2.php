78d536a7fb38a556b1c777c242f3860a15309b3f
Greg Nemes <mail@gregnemes.com>
2019-11-09 08:22:14 -0500

    added address to menu

--- themes/work-shop/menu-2.php
+++ themes/work-shop/menu-2.php
@@ -1,47 +1,49 @@
 	
-	<nav id="menu" class="off">
-		<!-- MENU 2 -->
-		<div class="container">
-	
-			<div class="row mt">
-				<div class="col-sm-4 col-sm-offset-1 menu-about m30 menu-mobile-nav visible-xs">
-					<ul class="">
-						<li><a href="<?php bloginfo('url'); ?>/about" class=" h2 bold">About</a></li>
-						<li><a href="<?php bloginfo('url'); ?>/about#contact" class=" h2 bold">Contact</a></li>
-						<li class="hidden"><a href="<?php bloginfo('url'); ?>/jobs" class=" h2 bold">Jobs</a></li>
-						<li><a href="<?php bloginfo('url'); ?>/projects" class=" h2 bold">Projects</a></li>
-						<li class="hidden"><a href="<?php bloginfo('url'); ?>/apprenticeship" class="h2 bold">Summer Apprenticeship Program</a></li>
-					</ul>
-				</div>
-			
-			
-				<div class="col-sm-5 col-sm-offset-1 menu-projects mt">
-					<h2 class=" m30 hidden-xs"><a href="<?php bloginfo('url'); ?>/projects" class="h2 bold">Projects</a></h2>
-					<div class="row">
-						<?php get_template_part('projects','menu'); ?>		
-					</div>		
-				</div>
-				
-				
-				<div class="col-sm-4 col-sm-offset-1 menu-about hidden-xs">
-					<h2 class="m30">We're an multidisciplinary design studio comprised of designers, makers, thinkers, and creative technologists. </h2>
-
-					<ul class="">
-						<li><a href="<?php bloginfo('url'); ?>/about" class=" h2 bold">About</a></li>
-						<li><a href="<?php bloginfo('url'); ?>/about#contact" class=" h2 bold">Contact</a></li>
-						<li class="hidden"><a href="<?php bloginfo('url'); ?>/jobs" class=" h2 bold">Jobs</a></li>
-						<li class="hidden"><a href="<?php bloginfo('url'); ?>/apprenticeship" class="h2 bold">Summer Apprenticeship Program</a></li>
-					</ul>
-					<address>	
-						<h4>204 Westminster Street, Floor 3</h4>
-						<h4>Providence, RI 02903</h4>						
-						<h4><a href="mailto:info@workshop.co" target="_blank">info@workshop.co</a></h4>
-						<h4>p. 440-915-2393</h4>
-					</address>	
-				</div>
-				
-			</div>			
-					
-		</div>
+<nav id="menu" class="off">
+	<!-- MENU 2 -->
+	<div class="container">
 		
-	</nav>
+		<div class="row mt">
+			<div class="col-sm-4 col-sm-offset-1 menu-about m30 menu-mobile-nav visible-xs">
+				<ul class="">
+					<li><a href="<?php bloginfo('url'); ?>/about" class=" h2 bold">About</a></li>
+					<li><a href="<?php bloginfo('url'); ?>/about#contact" class=" h2 bold">Contact</a></li>
+					<li class="hidden"><a href="<?php bloginfo('url'); ?>/jobs" class=" h2 bold">Jobs</a></li>
+					<li><a href="<?php bloginfo('url'); ?>/projects" class=" h2 bold">Projects</a></li>
+					<li class="hidden"><a href="<?php bloginfo('url'); ?>/apprenticeship" class="h2 bold">Summer Apprenticeship Program</a></li>
+				</ul>
+			</div>
+			
+			
+			<div class="col-sm-5 col-sm-offset-1 menu-projects mt">
+				<h2 class=" m30 hidden-xs"><a href="<?php bloginfo('url'); ?>/projects" class="h2 bold">Projects</a></h2>
+				<div class="row">
+					<?php get_template_part('projects','menu'); ?>		
+				</div>		
+			</div>
+			
+			
+			<div class="col-sm-4 col-sm-offset-1 menu-about hidden-xs">
+				<h2 class="m30">We're an multidisciplinary design studio comprised of designers, makers, thinkers, and creative technologists. </h2>
+
+				<ul class="">
+					<li><a href="<?php bloginfo('url'); ?>/about" class=" h2 bold">About</a></li>
+					<li><a href="<?php bloginfo('url'); ?>/about#contact" class=" h2 bold">Contact</a></li>
+					<li class="hidden"><a href="<?php bloginfo('url'); ?>/jobs" class=" h2 bold">Jobs</a></li>
+					<li class="hidden"><a href="<?php bloginfo('url'); ?>/apprenticeship" class="h2 bold">Summer Apprenticeship Program</a></li>
+				</ul>
+				<address>	
+					<h4>Work-Shop Design Studio</h4>
+					<h4>c/o Social Enterprise Greenhouse</h4>
+					<h4>10 Davol Sq, Suite 100</h4>
+					<h4>Providence, RI 02903</h4>						
+					<h4><a href="mailto:info@workshop.co" target="_blank">info@workshop.co</a></h4>
+					<h4>p. 440-915-2393</h4>
+				</address>	
+			</div>
+			
+		</div>			
+		
+	</div>
+	
+</nav>
