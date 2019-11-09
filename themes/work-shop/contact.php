a3fd0dfa0a108faf33122a52f36437e2618670b5
Greg Nemes <mail@gregnemes.com>
2019-11-09 08:21:00 -0500

    changed address

--- themes/work-shop/contact.php
+++ themes/work-shop/contact.php
@@ -4,7 +4,7 @@
 	<div class="block-body">
 		<div class="container">
 			<div class="row centered">
-			
+
 				<div class="col-sm-8 col-sm-offset-2">
 					<h1 class="centered m60">We want to work with you.</h1>
 				</div>	
@@ -13,15 +13,15 @@
 					<div class="contact-button-broken">
 						<h2 class="centered"><a href="#contact" data-toggle="modal" data-target="#contactModal" class="button">Send us a Message</a></h2>
 					</div>
-				
+
 				</div>
-			
+
 				<div class="col-sm-3 col-xs-12 centered">
 					<div class="contact-button-broken">
 						<h2 class="centered"><a href="#subscribe" data-toggle="modal" data-target="#subscribeModal" class="button">Get our Newsletter</a></h2>
 					</div>				
 				</div>										
-	
+
 			</div>
 		</div>
 	</div>
@@ -31,75 +31,67 @@
 <section class="block bg-light padded-less">
 	<div class="container">
 		<div class="row">
-				
-				<div class="col-sm-3">
-					<address>
-						<h3 class="bold uppercase m30 brand">Work-Shop</h3>				
-						<h4><a href="<?php bloginfo('url'); ?>/projects" target="_blank" class="dark">Projects</a></h4>	
-						<h4><a href="<?php bloginfo('url'); ?>/about" target="_blank" class="dark">About</a></h4>				
-						<h4><a href="http://facebook.com/workshopri" target="_blank" class="dark">Facebook</a></h4>				
-						<h4><a href="http://instagram.com/workshop.design.studio" target="_blank" class="dark">Instagram</a></h4>
-						<h4 class=""><a href="<?php bloginfo('url'); ?>/jobs" class="dark">Jobs</a></h4>
-						<h4 class="hidden"><a href="<?php bloginfo('url'); ?>/current" target="_blank" class="dark">Current</a></h4>
-					</address>	
-				</div>
-		
-				<div class="col-sm-3">
-					<address>
-						<h3 class="bold uppercase m30 brand">Contact</h3>
-						
-						<h4><a href="mailto:info@workshop.co" target="_blank" class="dark">info@workshop.co</a></h4>
-						<h4>p. 440.915.2393</h4>
-					</address>	
-				</div>
+
+			<div class="col-sm-3">
+				<address>
+					<h3 class="bold uppercase m30 brand">Work-Shop</h3>				
+					<h4><a href="<?php bloginfo('url'); ?>/projects" target="_blank" class="dark">Projects</a></h4>	
+					<h4><a href="<?php bloginfo('url'); ?>/about" target="_blank" class="dark">About</a></h4>				
+					<h4><a href="http://facebook.com/workshopri" target="_blank" class="dark">Facebook</a></h4>				
+					<h4><a href="http://instagram.com/workshop.design.studio" target="_blank" class="dark">Instagram</a></h4>
+					<h4 class=""><a href="<?php bloginfo('url'); ?>/jobs" class="dark">Jobs</a></h4>
+					<h4 class="hidden"><a href="<?php bloginfo('url'); ?>/current" target="_blank" class="dark">Current</a></h4>
+				</address>	
+			</div>
+
+			<div class="col-sm-3">
+				<address>
+					<h3 class="bold uppercase m30 brand">Contact</h3>
+
+					<h4><a href="mailto:info@workshop.co" target="_blank" class="dark">info@workshop.co</a></h4>
+					<h4>p. 440.915.2393</h4>
+				</address>	
+			</div>
 			
-				<div class="col-sm-3">
-					<address>
-						<h3 class="uppercase bold m30 brand">Office</h3>
-											
-						<h4>204 Westminster Street, Floor 3</h4>
-						<h4>Providence, RI 02903</h4>						
-					</address>				
-				</div>	
-				
-				<div class="col-sm-3 hidden">
-					<address>
-						<h3 class="uppercase bold m30 brand">Shop</h3>
-											
-						<h4>160 Narragansett Ave, Unit 2</h4>
-						<h4>Providence, RI 02907</h4>						
-					</address>					
-				</div>			
-		
-		
+			<div class="col-sm-3">
+				<address>
+					<h3 class="uppercase bold m30 brand">Office</h3>
+
+					<h4>Work-Shop Design Studio</h4>
+					<h4>c/o Social Enterprise Greenhouse</h4>
+					<h4>10 Davol Sq, Suite 100</h4>
+					<h4>Providence, RI 02903</h4>						
+				</address>				
+			</div>	
+
 		</div>
 	</div>
 </section>
 
 <div class="modal fade" id="contactModal" role="dialog" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close icon" data-dismiss="modal" aria-hidden="true" data-icon="x"></button>
-          <h3 class="modal-title centered">We'd love to hear from you.</h3>
-        </div>
-        <div class="modal-body">
-			<?php  gravity_form( 2, $display_title=false, $display_description=false, $display_inactive=false, $field_values=null, $ajax=true,$tabindex=1000); ?>	 
-		</div>
-      </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
-  </div><!-- /.modal -->
-  
-  <div class="modal fade" id="subscribeModal" role="dialog" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close icon" data-dismiss="modal" aria-hidden="true" data-icon="x"></button>
-          <h3 class="modal-title centered">We'll keep you up to date</h3>
-        </div>
-        <div class="modal-body">
-			<?php  gravity_form( 3, $display_title=false, $display_description=false, $display_inactive=false, $field_values=null, $ajax=true,$tabindex=2000); ?>	        
-		</div>
-      </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close icon" data-dismiss="modal" aria-hidden="true" data-icon="x"></button>
+				<h3 class="modal-title centered">We'd love to hear from you.</h3>
+			</div>
+			<div class="modal-body">
+				<?php  gravity_form( 2, $display_title=false, $display_description=false, $display_inactive=false, $field_values=null, $ajax=true,$tabindex=1000); ?>	 
+			</div>
+		</div><!-- /.modal-content -->
+	</div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+<div class="modal fade" id="subscribeModal" role="dialog" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close icon" data-dismiss="modal" aria-hidden="true" data-icon="x"></button>
+				<h3 class="modal-title centered">We'll keep you up to date</h3>
+			</div>
+			<div class="modal-body">
+				<?php  gravity_form( 3, $display_title=false, $display_description=false, $display_inactive=false, $field_values=null, $ajax=true,$tabindex=2000); ?>	        
+			</div>
+		</div><!-- /.modal-content -->
+	</div><!-- /.modal-dialog -->
   </div><!-- /.modal -->
